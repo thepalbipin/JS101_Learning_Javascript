a05313a191d0e347933312c3d36787b73c7b3b92
Bipin <bipinpal737@gmail.com>
2023-10-25 11:01:47 +0530

    IW Introduction of JS

---
+++ Day1/IW03.js
@@ -0,0 +1,8 @@
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+var name ="Bipin";
+var age="27";
+console.log(name);
+console.log(age);
---
+++ Day1/IW04.js
@@ -0,0 +1,24 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let name="Bipin";
+let school="APS";
+let grade="12th";
+let section="A";
+let roll_no=12;
+let physics=75;
+let chemistry=60;
+let mathematics=80;
+
+console.log("###################### REPORT CARD #######################");
+console.log("Name:",name,"                                             #");
+console.log("School:",school,"                                             #");
+console.log("Grade:",grade,"                                             #");
+console.log("Section:",section,"                                              #");
+console.log("Roll no:",roll_no,"                                             #");
+console.log("Physics:",physics,"                                             #");
+console.log("Chemistry:",chemistry,"                                           #");
+console.log("Mathematics:",mathematics,"                                         #");
+console.log("####################### ----x---- ########################");
